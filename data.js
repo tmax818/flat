d6306bf91224c8a9a4bfd1b4d5da4281404d0ef7
tmax818 <tylermaxwell661@gmail.com>
2021-01-07 07:51:03 -0800

    update data.js

--- data.js
+++ data.js
@@ -1,31 +1,55 @@
 const data = [
-    /// Welcome
+    /// Welcome1
     { title: "Welcome To Learn", link: "welcome-to-learn-verified", lab: false, readCount: 0 },
     { title: "Navigating Curriculum On Learn", link: "prework-intro-navigating-curriculum-on-learn-ide", lab: false },
     { title: "Ask A Question", link: "prework-intro-ask-a-question", lab: false },
-    /// Git GitHub
+    /// Git GitHub2
     { title: "Introduction To Version Control", link: "git-version-control-introduction-to-version-control", lab: false },
     { title: "Version Control Basics", link: "git-version-control-git-basics", lab: false },
     { title: "Getting Code With Git", link: "git-version-control-getting-code-with-git", lab: false },
     { title: "Pushing Code With Git", link: "git-version-control-pushing-code-with-git", lab: false },
     { title: "Git Basics Lab", link: "git-basics-lab-v-000", lab: true },
     { title: "Thinking Ahead", link: "careers-online-presence-github", lab: false },
+    ///2
     { title: "GitHub Pull Request Basics", link: "github-pull-request-basics", lab: false },
     { title: "Git Collaboration", link: "git-collaboration-readme", lab: false },
-    { title: "Git Collaboration Quiz", link: "", lab: false },
-    { title: "Git Basics Quiz", link: "", lab: false },
+    { title: "Git Collaboration Quiz", link: "git-collaboration-quiz", lab: false },
+    { title: "Git Basics Quiz", link: "git-github-learn-quiz", lab: false },
+    ///3
     { title: "TDD Rspec And Learn", link: "intro-to-tdd-rspec-and-learn-v-000", lab: true },
-    /// Intro to Ruby Dev
+    /// Intro to Ruby Dev3
+    { title: "Intro to Tic Tac Toe", link: "intro-to-tic-tac-toe-rb", lab: false },
+    { title: "MINSWAN- Yukihuro Matsumoto", link: "matz-readme", lab: false },
+    { title: "What is a Program?", link: "ruby-lecture-intro-what-is-a-program", lab: false },
     { title: "Hello World", link: "hello-world-ruby-v-000", lab: true },
+    ////2
+    { title: "Intro to IRB", link: "irb-readme", lab: false },
     { title: "Reading Error Messages", link: "ruby-lecture-reading-error-messages-v-000", lab: true },
+    ////3
     { title: "Variable Assignment Lab", link: "ruby-variable-assignment-v-000", lab: true },
     { title: "Tic Tac Toe Board", link: "ttt-2-board-rb-v-000", lab: true },
+    ////
     { title: "Intro to Methods", link: "ruby-methods-readme-v-000", lab: true },
     { title: "String Interpolation", link: "interpolation-readme", lab: false },
     { title: "Display Tic Tac Toe Board Example", link: "ttt-3-display_board-example-v-000", lab: true },
+    ////
     { title: "Intro to CLI Applications", link: "intro-to-cli-applications", lab: false },
     { title: "CLI Applications in Ruby", link: "ruby-cli-applications-readme", lab: false },
     { title: "gets CLI Input", link: "ruby-gets-input-v-000", lab: true },
+    { title: "Greeting CLI Lab", link: "greeting-cli-v-000", lab: true },
+    { title: "Tic Tac Toe Move", link: "ttt-5-move-rb-v-000", lab: true },
+
+    // OO Ruby6
+    { title: "Intro to Object Orientation", link: "", lab: false },
+    { title: "OO Ruby Video: Object Orientation Overview", link: "oo-ruby-video-object-orientation-overview", lab: false },
+    ////
+    { title: "Intro to Classes and Instances", link: "ruby-intro-to-classes-and-instances", lab: false },
+    { title: "Classes and Instances Lab", link: "classes-and-instances-lab-ruby-v-000", lab: true },
+    ////
+    { title: "Instance Methods", link: "instance-methods-readme-ruby", lab: false },
+    { title: "Instance Methods Lab", link: "instance-methods-lab-ruby-v-000", lab: true },
+
+
 
 ]
 
