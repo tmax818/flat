aa4b1e56fc3df3256867290be785d6babd56fd18
tmax818 <tylermaxwell661@gmail.com>
2021-01-08 06:37:55 -0800

    test cl

--- cloneRepos.js
+++ cloneRepos.js
@@ -4,10 +4,10 @@ const data = require("./data")
 
 data.forEach(datum => {
     if (datum.lab) {
-        shell.exec(`git clone https://github.com/learn-co-students/${datum.link}.git labs/${datum.link}`)
+        shell.exec(`git clone https://github.com/learn-co-students/${datum.link}.git repos/${datum.link}`)
         shell.exec(`git clone https://github.com/tmax818/${datum.link}.git forks/${datum.link}`)
     } else {
-        shell.exec(`git clone https://github.com/learn-co-curriculum/${datum.link}.git lectures/${datum.link}`)
+        shell.exec(`git clone https://github.com/learn-co-curriculum/${datum.link}.git repos/${datum.link}`)
         shell.exec(`git clone https://github.com/tmax818/${datum.link}.git forks/${datum.link}`)
     }
 })
@@ -15,8 +15,3 @@ data.forEach(datum => {
 
 
 
-
-
-
-
-//shell.exec(`git clone https://github.com/learn-co-curriculum/ruby-cli-applications-readme.git lectures/ruby-cli-applications-readme`);
