f5d5545879cc4600d748108eb36273a3ebddee6c
tmax818 <tylermaxwell661@gmail.com>
2021-01-24 10:02:41 -0800

    update

--- bin/cloneRepos.js
+++ bin/cloneRepos.js
@@ -2,9 +2,9 @@ const shell = require('shelljs');
 const data = require("../data/data")
 const repo = "https://github.com/tmax818"
 
-
+//git clone --single-branch --branch <branchname> <remote-repo>
 data.forEach(datum => {
-    shell.exec(`git clone ${repo}/${datum.link}.git forks/${datum.link}`)
+    shell.exec(`git clone --single-branch --branch tyler ${repo}/${datum.link}.git forks/${datum.link}`)
 })
 
 
